691ea835b7754507f6dac7caf5e84b3915478ebf
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-06-13 17:59:58 -0300

    ClassValidatorFields - unit tests - part 2

--- src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
+++ src/shared/domain/validators/__tests__/unit/class-validator-fields.spec.ts
@@ -25,4 +25,15 @@ describe('ClassValidatorFields unit tests', () => {
     expect(sut.validatedData).toBeNull()
     expect(sut.errors).toStrictEqual({ field: ['test error'] })
   })
+
+  it('Should validate without errors', () => {
+    const spyValidateSync = jest.spyOn(libClassValidator, 'validateSync')
+    spyValidateSync.mockReturnValue([])
+    const sut = new StubClassValidatorFields()
+
+    expect(sut.validate({ field: 'value' })).toBeTruthy()
+    expect(spyValidateSync).toHaveBeenCalled()
+    expect(sut.validatedData).toStrictEqual({ field: 'value' })
+    expect(sut.errors).toBeNull()
+  })
 })
