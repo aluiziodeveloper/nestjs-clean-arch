fa9739dafb6e2ff61c1d9f7fe822158e570d67e4
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-06-13 14:49:55 -0300

    UserEntity - constructor unit tests

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,23 @@
+import { faker } from '@faker-js/faker'
+import { UserEntity, UserProps } from '../../user.entity'
+
+describe('UserEntity unit tests', () => {
+  let props: UserProps
+  let sut: UserEntity
+
+  beforeEach(() => {
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    }
+    sut = new UserEntity(props)
+  })
+
+  it('Constructor method', () => {
+    expect(sut.props.name).toEqual(props.name)
+    expect(sut.props.email).toEqual(props.email)
+    expect(sut.props.password).toEqual(props.password)
+    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  })
+})
--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserProps = {
+  name: string
+  email: string
+  password: string
+  createdAt?: Date
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps) {
+    this.props.createdAt = this.props.createdAt ?? new Date()
+  }
+}
