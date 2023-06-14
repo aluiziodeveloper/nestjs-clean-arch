3bb3db6f01ee8ec89eb2dec8b63a8b6f9ac6db43
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-06-13 22:14:11 -0300

    Integration tests - valid fields on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -95,5 +95,14 @@ describe('UserEntity integration tests', () => {
       }
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
     })
+
+    it('Should a valid user', () => {
+      expect.assertions(0)
+
+      const props: UserProps = {
+        ...UserDataBuilder({}),
+      }
+      new UserEntity(props)
+    })
   })
 })
