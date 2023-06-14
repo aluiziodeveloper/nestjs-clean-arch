7ed68dcc8f06c2986c3b56a696de8230b10fa78b
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-06-13 22:26:59 -0300

    Integration tests - valid fields on update user

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -1,6 +1,7 @@
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
 import { UserEntity, UserProps } from '../../user.entity'
 import { EntityValidationError } from '@/shared/domain/errors/validation-error'
+import { Entity } from '@/shared/domain/entities/entity'
 
 describe('UserEntity integration tests', () => {
   describe('Constructor method', () => {
@@ -103,6 +104,29 @@ describe('UserEntity integration tests', () => {
         ...UserDataBuilder({}),
       }
       new UserEntity(props)
+    })
+  })
+
+  describe('Update method', () => {
+    it('Should throw an error when update a user with invalid name', () => {
+      const entity = new UserEntity(UserDataBuilder({}))
+      expect(() => entity.update(null)).toThrowError(EntityValidationError)
+      expect(() => entity.update('')).toThrowError(EntityValidationError)
+      expect(() => entity.update(10 as any)).toThrowError(EntityValidationError)
+      expect(() => entity.update('a'.repeat(256))).toThrowError(
+        EntityValidationError,
+      )
+    })
+
+    it('Should a valid user', () => {
+      expect.assertions(0)
+
+      const props: UserProps = {
+        ...UserDataBuilder({}),
+      }
+
+      const entity = new UserEntity(props)
+      entity.update('other name')
     })
   })
 })
