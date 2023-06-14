8d087960057d0b3e40e4b4e20df0d9e9ef756dfb
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-06-13 21:56:23 -0300

    Integration tests - validate email field on create user

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -19,7 +19,39 @@ describe('UserEntity integration tests', () => {
 
       props = {
         ...UserDataBuilder({}),
+        name: 10 as any,
+      }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
         name: 'a'.repeat(256),
+      }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+    })
+
+    it('Should throw an error when creating a user with invalid email', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        email: null,
+      }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        email: '',
+      }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 10 as any,
+      }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        email: 'a'.repeat(256),
       }
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
     })
