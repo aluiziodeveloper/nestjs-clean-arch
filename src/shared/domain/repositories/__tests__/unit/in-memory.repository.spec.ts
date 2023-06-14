49fb8f016d6794cfd1348fd814913c8c33b532fd
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-06-14 10:41:08 -0300

    InMemoryRepository unit tests - findAll method

--- src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -36,4 +36,11 @@ describe('InMemoryRepository unit tests', () => {
     const result = await sut.findById(entity._id)
     expect(entity.toJSON()).toStrictEqual(result.toJSON())
   })
+
+  it('Should returns all entities', async () => {
+    const entity = new StubEntity({ name: 'test name', price: 50 })
+    await sut.insert(entity)
+    const result = await sut.findAll()
+    expect([entity]).toStrictEqual(result)
+  })
 })
