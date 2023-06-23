a38c909b7c8779ec922f05d9ce5a857db7d3dad9
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-06-23 01:09:54 -0300

    Adding SearchParams class - part 2

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -30,31 +30,60 @@ export class SearchParams {
     return this._page
   }
 
-  private set page(value: number) {}
+  private set page(value: number) {
+    let _page = +value
+    if (Number.isNaN(_page) || _page <= 0 || parseInt(_page as any) !== _page) {
+      _page = 1
+    }
+    this._page = _page
+  }
 
   get perPage() {
     return this._perPage
   }
 
-  private set perPage(value: number) {}
+  private set perPage(value: number) {
+    let _perPage = +value
+    if (
+      Number.isNaN(_perPage) ||
+      _perPage <= 0 ||
+      parseInt(_perPage as any) !== _perPage
+    ) {
+      _perPage = this._perPage
+    }
+    this._page = _perPage
+  }
 
   get sort() {
     return this._sort
   }
 
-  private set sort(value: string | null) {}
+  private set sort(value: string | null) {
+    this._sort =
+      value === null || value === undefined || value === '' ? null : `${value}`
+  }
 
   get sortDir() {
     return this._sortDir
   }
 
-  private set sortDir(value: string | null) {}
+  private set sortDir(value: string | null) {
+    if (!this.sort) {
+      this._sortDir = null
+      return
+    }
+    const dir = `${value}`.toLowerCase()
+    this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir
+  }
 
   get filter() {
     return this._filter
   }
 
-  private set filter(value: string | null) {}
+  private set filter(value: string | null) {
+    this._filter =
+      value === null || value === undefined || value === '' ? null : `${value}`
+  }
 }
 
 export interface SearchableRepositoryInterface<
