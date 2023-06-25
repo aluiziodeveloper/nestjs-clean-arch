2ab75012b5f11f2c2ecb152804509967d480252e
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-06-24 23:41:19 -0300

    Unit tests SearchParams class - part 1

---
+++ src/shared/domain/repositories/__tests__/unit/searchable-repository-contracts.spec.ts
@@ -0,0 +1,56 @@
+import { SearchParams } from '../../searchable-repository-contracts'
+
+describe('Searchable Repository unit tests', () => {
+  describe('SearchParams tests', () => {
+    it('page prop', () => {
+      const sut = new SearchParams()
+      expect(sut.page).toEqual(1)
+
+      const params = [
+        { page: null as any, expected: 1 },
+        { page: undefined as any, expected: 1 },
+        { page: '' as any, expected: 1 },
+        { page: 'test' as any, expected: 1 },
+        { page: 0, expected: 1 },
+        { page: -1, expected: 1 },
+        { page: 5.5, expected: 1 },
+        { page: true, expected: 1 },
+        { page: false, expected: 1 },
+        { page: {}, expected: 1 },
+        { page: 1, expected: 1 },
+        { page: 2, expected: 2 },
+      ]
+
+      params.forEach(i => {
+        expect(new SearchParams({ page: i.page }).page).toBe(i.expected)
+      })
+    })
+
+    it('perPage prop', () => {
+      const sut = new SearchParams()
+      expect(sut.perPage).toEqual(15)
+
+      const params = [
+        { perPage: null as any, expected: 15 },
+        { perPage: undefined as any, expected: 15 },
+        { perPage: '' as any, expected: 15 },
+        { perPage: 'test' as any, expected: 15 },
+        { perPage: 0, expected: 15 },
+        { perPage: -1, expected: 15 },
+        { perPage: 5.5, expected: 15 },
+        { perPage: true, expected: 15 },
+        { perPage: false, expected: 15 },
+        { perPage: {}, expected: 15 },
+        { perPage: 1, expected: 1 },
+        { perPage: 2, expected: 2 },
+        { perPage: 25, expected: 25 },
+      ]
+
+      params.forEach(i => {
+        expect(new SearchParams({ perPage: i.perPage }).perPage).toBe(
+          i.expected,
+        )
+      })
+    })
+  })
+})
--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -18,12 +18,12 @@ export class SearchParams {
   protected _sortDir: SortDirection | null
   protected _filter: string | null
 
-  constructor(props: SearchProps) {
-    this._page = props.page
-    this._perPage = props.perPage
-    this._sort = props.sort
-    this._sortDir = props.sortDir
-    this._filter = props.filter
+  constructor(props: SearchProps = {}) {
+    this.page = props.page
+    this.perPage = props.perPage
+    this.sort = props.sort
+    this.sortDir = props.sortDir
+    this.filter = props.filter
   }
 
   get page() {
@@ -43,7 +43,7 @@ export class SearchParams {
   }
 
   private set perPage(value: number) {
-    let _perPage = +value
+    let _perPage = value === (true as any) ? this._perPage : +value
     if (
       Number.isNaN(_perPage) ||
       _perPage <= 0 ||
@@ -51,7 +51,7 @@ export class SearchParams {
     ) {
       _perPage = this._perPage
     }
-    this._page = _perPage
+    this._perPage = _perPage
   }
 
   get sort() {
