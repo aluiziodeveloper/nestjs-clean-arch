617faace80222887562f1f877d6ded00a68b0f2b
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-07-05 20:24:09 -0300

    Adding usecase SignupUseCase - part 1

---
+++ src/users/application/usecases/signup.usecase.ts
@@ -0,0 +1,19 @@
+export namespace SignupUseCase {
+  export type Input = {
+    name: string
+    email: string
+    password: string
+  }
+
+  export type Output = {
+    id: string
+    name: string
+    email: string
+    password: string
+    createdAt: Date
+  }
+
+  export class UseCase {
+    async execute(input: Input): Promise<Output> {}
+  }
+}
