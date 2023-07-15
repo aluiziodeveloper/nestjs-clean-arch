3cc06d3d705fe8afd581954775c59d431eee7fc2
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-07-14 22:38:10 -0300

    Configuring UsersController

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -6,37 +6,84 @@ import {
   Patch,
   Param,
   Delete,
+  Inject,
+  HttpCode,
+  Query,
+  Put,
 } from '@nestjs/common'
-import { UsersService } from './users.service'
 import { SignupDto } from './dtos/signup.dto'
 import { UpdateUserDto } from './dtos/update-user.dto'
+import { SignupUseCase } from '../application/usecases/signup.usecase'
+import { SigninUseCase } from '../application/usecases/signin.usecase'
+import { UpdateUserUseCase } from '../application/usecases/update-user.usecase'
+import { UpdatePasswordUseCase } from '../application/usecases/update-password.usecase'
+import { DeleteUserUseCase } from '../application/usecases/delete-user.usecase'
+import { GetUserUseCase } from '../application/usecases/getuser.usecase'
+import { ListUsersUseCase } from '../application/usecases/listusers.usecase'
+import { SigninDto } from './dtos/signin.dto'
+import { ListUsersDto } from './dtos/list-users.dto'
+import { UpdatePasswordDto } from './dtos/update-password.dto'
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  @Inject(SignupUseCase.UseCase)
+  private signupUseCase: SignupUseCase.UseCase
+
+  @Inject(SigninUseCase.UseCase)
+  private signinUseCase: SigninUseCase.UseCase
+
+  @Inject(UpdateUserUseCase.UseCase)
+  private updateUserUseCase: UpdateUserUseCase.UseCase
+
+  @Inject(UpdatePasswordUseCase.UseCase)
+  private updatePasswordUseCase: UpdatePasswordUseCase.UseCase
+
+  @Inject(DeleteUserUseCase.UseCase)
+  private deleteUserUseCase: DeleteUserUseCase.UseCase
+
+  @Inject(GetUserUseCase.UseCase)
+  private getUserUseCase: GetUserUseCase.UseCase
+
+  @Inject(ListUsersUseCase.UseCase)
+  private listUsersUseCase: ListUsersUseCase.UseCase
 
   @Post()
-  create(@Body() signupDto: SignupDto) {
-    return this.usersService.create(signupDto)
+  async create(@Body() signupDto: SignupDto) {
+    return this.signupUseCase.execute(signupDto)
+  }
+
+  @HttpCode(200)
+  @Post('login')
+  async login(@Body() signinDto: SigninDto) {
+    return this.signinUseCase.execute(signinDto)
   }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll()
+  async search(@Query() searchParams: ListUsersDto) {
+    return this.listUsersUseCase.execute(searchParams)
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id)
+  async findOne(@Param('id') id: string) {
+    return this.getUserUseCase.execute({ id })
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.updateUserUseCase.execute({ id, ...updateUserDto })
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto)
+  async updatePassword(
+    @Param('id') id: string,
+    @Body() updatePasswordDto: UpdatePasswordDto,
+  ) {
+    return this.updatePasswordUseCase.execute({ id, ...updatePasswordDto })
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id)
+  async remove(@Param('id') id: string) {
+    await this.deleteUserUseCase.execute({ id })
   }
 }
--- src/users/infrastructure/users.module.ts
+++ src/users/infrastructure/users.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common'
-import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
 import { SignupUseCase } from '../application/usecases/signup.usecase'
 import { UserInMemoryRepository } from './database/in-memory/repositories/user-in-memory.repository'
@@ -16,7 +15,6 @@ import { DeleteUserUseCase } from '../application/usecases/delete-user.usecase'
 @Module({
   controllers: [UsersController],
   providers: [
-    UsersService,
     {
       provide: 'UserRepository',
       useClass: UserInMemoryRepository,
