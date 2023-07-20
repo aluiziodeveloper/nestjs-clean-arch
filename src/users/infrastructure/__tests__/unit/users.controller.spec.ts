fed299317d243a1fac2278d2ba5de13b04b79459
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-07-20 20:37:55 -0300

    UsersController unit tests - updatePassword method

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -6,6 +6,8 @@ import { SigninUseCase } from '@/users/application/usecases/signin.usecase'
 import { SigninDto } from '../../dtos/signin.dto'
 import { UpdateUserUseCase } from '@/users/application/usecases/update-user.usecase'
 import { UpdateUserDto } from '../../dtos/update-user.dto'
+import { UpdatePasswordUseCase } from '@/users/application/usecases/update-password.usecase'
+import { UpdatePasswordDto } from '../../dtos/update-password.dto'
 
 describe('UsersController unit tests', () => {
   let sut: UsersController
@@ -71,5 +73,23 @@ describe('UsersController unit tests', () => {
     const result = await sut.update(id, input)
     expect(output).toMatchObject(result)
     expect(mockUpdateUserUseCase.execute).toHaveBeenCalledWith({ id, ...input })
+  })
+
+  it('should update a users password', async () => {
+    const output: UpdatePasswordUseCase.Output = props
+    const mockUpdatePasswordUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['updatePasswordUseCase'] = mockUpdatePasswordUseCase as any
+    const input: UpdatePasswordDto = {
+      password: 'new password',
+      oldPassword: 'old password',
+    }
+    const result = await sut.updatePassword(id, input)
+    expect(output).toMatchObject(result)
+    expect(mockUpdatePasswordUseCase.execute).toHaveBeenCalledWith({
+      id,
+      ...input,
+    })
   })
 })
