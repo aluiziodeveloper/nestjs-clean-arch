7afc393ca0d226e1ac5a6a9024afb9ee9b87b70a
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-07-26 19:46:25 -0300

    Running prisma migrate

---
+++ src/shared/infrastructure/database/prisma/migrations/20230726224003_create_users_table/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(100) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
