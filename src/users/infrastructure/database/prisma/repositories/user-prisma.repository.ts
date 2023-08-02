1590340fa2538e61b1e07452d928f36ee2360a4e
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-08-02 09:18:39 -0300

    PrismaUserRepository - implementing search method

--- src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
+++ src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
@@ -5,7 +5,7 @@ import { UserRepository } from '@/users/domain/repositories/user.repository'
 import { UserModelMapper } from '../models/user-model.mapper'
 
 export class UserPrismaRepository implements UserRepository.Repository {
-  sortableFields: string[]
+  sortableFields: string[] = ['name', 'createdAt']
 
   constructor(private prismaService: PrismaService) {}
 
@@ -17,10 +17,50 @@ export class UserPrismaRepository implements UserRepository.Repository {
     throw new Error('Method not implemented.')
   }
 
-  search(
+  async search(
     props: UserRepository.SearchParams,
   ): Promise<UserRepository.SearchResult> {
-    throw new Error('Method not implemented.')
+    const sortable = this.sortableFields?.includes(props.sort) || false
+    const orderByField = sortable ? props.sort : 'createdAt'
+    const orderByDir = sortable ? props.sortDir : 'desc'
+
+    const count = await this.prismaService.user.count({
+      ...(props.filter && {
+        where: {
+          name: {
+            contains: props.filter,
+            mode: 'insensitive',
+          },
+        },
+      }),
+    })
+
+    const models = this.prismaService.user.findMany({
+      ...(props.filter && {
+        where: {
+          name: {
+            contains: props.filter,
+            mode: 'insensitive',
+          },
+        },
+        orderBy: {
+          [orderByField]: orderByDir,
+        },
+        skip:
+          props.page && props.page > 0 ? (props.page - 1) * props.perPage : 1,
+        take: props.perPage && props.perPage > 0 ? props.perPage : 15,
+      }),
+    })
+
+    return new UserRepository.SearchResult({
+      items: (await models).map(model => UserModelMapper.toEntity(model)),
+      total: count,
+      currentPage: props.page,
+      perPage: props.perPage,
+      sort: orderByField,
+      sortDir: orderByDir,
+      filter: props.filter,
+    })
   }
 
   async insert(entity: UserEntity): Promise<void> {
