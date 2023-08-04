5cb9996eb8ed18c4c4d76d497e32c4318aa30e6d
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-08-04 08:17:14 -0300

    Integration tests of search method - part 3

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
@@ -86,12 +86,16 @@ describe('UserPrismaRepository integration tests', () => {
       })
 
       const searchOutput = await sut.search(new UserRepository.SearchParams())
+      const items = searchOutput.items
 
       expect(searchOutput).toBeInstanceOf(UserRepository.SearchResult)
       expect(searchOutput.total).toBe(16)
       expect(searchOutput.items.length).toBe(15)
       searchOutput.items.forEach(item => {
         expect(item).toBeInstanceOf(UserEntity)
+      })
+      items.reverse().forEach((item, index) => {
+        expect(`test${index + 1}@mail.com`).toBe(item.email)
       })
     })
   })
