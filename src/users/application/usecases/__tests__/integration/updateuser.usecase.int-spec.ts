909aa3b083084f927b8946512e2ddcd23619e027
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-08-07 17:20:16 -0300

    Adding UpdateUserUseCase integration tests

---
+++ src/users/application/usecases/__tests__/integration/updateuser.usecase.int-spec.ts
@@ -0,0 +1,50 @@
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module'
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests'
+import { UserPrismaRepository } from '@/users/infrastructure/database/prisma/repositories/user-prisma.repository'
+import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaClient } from '@prisma/client'
+import { NotFoundError } from '@/shared/domain/errors/not-found-error'
+import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+import { UpdateUserUseCase } from '../../update-user.usecase'
+
+describe('UpdateUserUseCase integration tests', () => {
+  const prismaService = new PrismaClient()
+  let sut: UpdateUserUseCase.UseCase
+  let repository: UserPrismaRepository
+  let module: TestingModule
+
+  beforeAll(async () => {
+    setupPrismaTests()
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule.forTest(prismaService)],
+    }).compile()
+    repository = new UserPrismaRepository(prismaService as any)
+  })
+
+  beforeEach(async () => {
+    sut = new UpdateUserUseCase.UseCase(repository)
+    await prismaService.user.deleteMany()
+  })
+
+  afterAll(async () => {
+    await module.close()
+  })
+
+  it('should throws error when entity not found', async () => {
+    await expect(() =>
+      sut.execute({ id: 'fakeId', name: 'fake name' }),
+    ).rejects.toThrow(new NotFoundError('UserModel not found using ID fakeId'))
+  })
+
+  it('should update a user', async () => {
+    const entity = new UserEntity(UserDataBuilder({}))
+    const model = await prismaService.user.create({
+      data: entity.toJSON(),
+    })
+
+    const output = await sut.execute({ id: entity._id, name: 'new name' })
+
+    expect(output.name).toBe('new name')
+  })
+})
