cac224f7486197127430a7384783afba90dcd801
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-08-07 15:58:58 -0300

    Refactor UserEntity unit tests

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -20,9 +20,9 @@ describe('UserEntity unit tests', () => {
   })
 
   it('Getter of name field', () => {
-    expect(sut.props.name).toBeDefined()
-    expect(sut.props.name).toEqual(props.name)
-    expect(typeof sut.props.name).toBe('string')
+    expect(sut.name).toBeDefined()
+    expect(sut.name).toEqual(props.name)
+    expect(typeof sut.name).toBe('string')
   })
 
   it('Setter of name field', () => {
@@ -32,15 +32,15 @@ describe('UserEntity unit tests', () => {
   })
 
   it('Getter of email field', () => {
-    expect(sut.props.email).toBeDefined()
-    expect(sut.props.email).toEqual(props.email)
-    expect(typeof sut.props.email).toBe('string')
+    expect(sut.email).toBeDefined()
+    expect(sut.email).toEqual(props.email)
+    expect(typeof sut.email).toBe('string')
   })
 
   it('Getter of password field', () => {
-    expect(sut.props.password).toBeDefined()
-    expect(sut.props.password).toEqual(props.password)
-    expect(typeof sut.props.password).toBe('string')
+    expect(sut.password).toBeDefined()
+    expect(sut.password).toEqual(props.password)
+    expect(typeof sut.password).toBe('string')
   })
 
   it('Setter of password field', () => {
@@ -49,9 +49,9 @@ describe('UserEntity unit tests', () => {
     expect(typeof sut.props.password).toBe('string')
   })
 
-  it('Getter of password field', () => {
-    expect(sut.props.createdAt).toBeDefined()
-    expect(sut.props.createdAt).toBeInstanceOf(Date)
+  it('Getter of createdAt field', () => {
+    expect(sut.createdAt).toBeDefined()
+    expect(sut.createdAt).toBeInstanceOf(Date)
   })
 
   it('Should update a user', () => {
