7946b31560aadbd03a7c48945160722cb7b5b599
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-08-08 10:50:17 -0300

    UserPresenter class unit tests - part 2

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,3 +1,4 @@
+import { instanceToPlain } from 'class-transformer'
 import { UserPresenter } from '../../user.presenter'
 
 describe('UserPresenter unit tests', () => {
@@ -9,14 +10,28 @@ describe('UserPresenter unit tests', () => {
     password: 'fake',
     createdAt,
   }
+  let sut: UserPresenter
+
+  beforeEach(() => {
+    sut = new UserPresenter(props)
+  })
 
   describe('constructor', () => {
     it('should be defined', () => {
-      const sut = new UserPresenter(props)
       expect(sut.id).toEqual(props.id)
       expect(sut.name).toEqual(props.name)
       expect(sut.email).toEqual(props.email)
       expect(sut.createdAt).toEqual(props.createdAt)
+    })
+  })
+
+  it('should presenter data', () => {
+    const output = instanceToPlain(sut)
+    expect(output).toStrictEqual({
+      id: 'e71c52a2-9710-4a96-a08e-144af4209b5d',
+      name: 'test name',
+      email: 'a@a.com',
+      createdAt: createdAt.toISOString(),
     })
   })
 })
