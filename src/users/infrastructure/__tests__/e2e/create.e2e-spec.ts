ca1f38b4bd594588692e904033db27007f90cee9
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-08-08 16:30:10 -0300

    UsersController e2e tests - create method - part 2

--- src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/create.e2e-spec.ts
@@ -55,5 +55,22 @@ describe('UsersController unit tests', () => {
       const serialized = instanceToPlain(presenter)
       expect(res.body.data).toStrictEqual(serialized)
     })
+
+    it('should return a error with 422 code when the request body is invalid', async () => {
+      const res = await request(app.getHttpServer())
+        .post('/users')
+        .send({})
+        .expect(422)
+      expect(res.body.error).toBe('Unprocessable Entity')
+      expect(res.body.message).toEqual([
+        'name should not be empty',
+        'name must be a string',
+        'email must be an email',
+        'email should not be empty',
+        'email must be a string',
+        'password should not be empty',
+        'password must be a string',
+      ])
+    })
   })
 })
