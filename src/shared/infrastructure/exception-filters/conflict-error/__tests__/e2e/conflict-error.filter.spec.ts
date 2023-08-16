e569e331c4ca57183ab2171975cc3324ce79f18b
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-08-16 20:48:20 -0300

    ConflictErrorFilter e2e tests

---
+++ src/shared/infrastructure/exception-filters/conflict-error/__tests__/e2e/conflict-error.filter.spec.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, INestApplication } from '@nestjs/common'
+import { ConflictErrorFilter } from '../../conflict-error.filter'
+import { Test, TestingModule } from '@nestjs/testing'
+import { ConflictError } from '@/shared/domain/errors/conflict-error'
+import request from 'supertest'
+
+@Controller('stub')
+class StubController {
+  @Get()
+  index() {
+    throw new ConflictError('Conflicting data')
+  }
+}
+
+describe('ConflictErrorFilter (e2e)', () => {
+  let app: INestApplication
+  let module: TestingModule
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      controllers: [StubController],
+    }).compile()
+    app = module.createNestApplication()
+    app.useGlobalFilters(new ConflictErrorFilter())
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await module.close()
+  })
+
+  it('should be defined', () => {
+    expect(new ConflictErrorFilter()).toBeDefined()
+  })
+
+  it('should catch a ConflictError', () => {
+    return request(app.getHttpServer()).get('/stub').expect(409).expect({
+      statusCode: 409,
+      error: 'Conflict',
+      message: 'Conflicting data',
+    })
+  })
+})
--- src/shared/infrastructure/exception-filters/conflict-error/conflict-error.filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ConflictErrorFilter } from './conflict-error.filter'
-
-describe('ConflictErrorFilter', () => {
-  it('should be defined', () => {
-    expect(new ConflictErrorFilter()).toBeDefined()
-  })
-})
