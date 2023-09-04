cad354bfb8a326f79b457b7113e6ad2945574f8e
Aluizio Developer <aluiziodeveloper@gmail.com>
2023-09-03 22:32:35 -0300

    package.json adjusts

---
+++ jest.unit.config.ts
@@ -0,0 +1,16 @@
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+}
