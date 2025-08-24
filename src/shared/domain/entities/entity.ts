38a69265e7537f417b98407ac72a943ac3d712e6
Aluizio Developer <68967867+aluiziodeveloper@users.noreply.github.com>
2025-08-24 14:15:49 -0300

    Update entity.ts

--- src/shared/domain/entities/entity.ts
+++ src/shared/domain/entities/entity.ts
@@ -1,22 +1,22 @@
-import { v4 as uuidv4 } from 'uuid'
+import { randomUUID as uuidv4 } from 'node:crypto';
 
 export abstract class Entity<Props = any> {
-  public readonly _id: string
-  public readonly props: Props
+  public readonly _id: string;
+  public readonly props: Props;
 
   constructor(props: Props, id?: string) {
-    this.props = props
-    this._id = id || uuidv4()
+    this.props = props;
+    this._id = id || uuidv4();
   }
 
   get id() {
-    return this._id
+    return this._id;
   }
 
-  toJSON(): Required<{ id: string } & Props> {
+  toJSON(): { id: string } & Props {
     return {
       id: this._id,
       ...this.props,
-    } as Required<{ id: string } & Props>
+    };
   }
 }
