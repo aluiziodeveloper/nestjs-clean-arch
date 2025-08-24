442183bf883acf40012176a26504903bdb5d25d3
Aluizio Developer <68967867+aluiziodeveloper@users.noreply.github.com>
2025-08-24 14:19:30 -0300

    Refactoring entity.ts

--- src/shared/domain/entities/entity.ts
+++ src/shared/domain/entities/entity.ts
@@ -13,10 +13,10 @@ export abstract class Entity<Props = any> {
     return this._id;
   }
 
-  toJSON(): { id: string } & Props {
+  toJSON(): Required<{ id: string } & Props> {
     return {
       id: this._id,
       ...this.props,
-    };
+    } as Required<{ id: string } & Props>;
   }
 }
